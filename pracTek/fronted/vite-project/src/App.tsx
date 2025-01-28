2eb6ad190d92eb547858b379322114b75fd95834
shulamitProjects <shulamit5665@gmail.com>
2025-01-28 11:27:49 +0200

    aingup

--- pracTek/fronted/vite-project/src/App.tsx
+++ pracTek/fronted/vite-project/src/App.tsx
@@ -1,12 +1,22 @@
 // import React from 'react';
 import './App.css';
-import Dashboard from "./components/A/Dashboard";
+import { PrimeReactProvider } from 'primereact/api'; // PrimeReact Context Provider
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Routing components
+import SignUp from './components/B/SingUp'; // SignUp component
+import AddCustomers from './components/DTC/AddCustomers'; // CreateCompany component
+// import Dashboard from "./components/A/Dashboard";
 const App: React.FC = () => {
   return (
-   <div>
-    <Dashboard></Dashboard>
-   </div>
+    <PrimeReactProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<SignUp />} />
+          <Route path="/AddCustomers" element={<AddCustomers />} />
+          {/* <Route path="/CreateCompany" element={<CreateCompany />} /> */}
+          {/* Add more routes as needed */}
+        </Routes>
+      </Router>
+    </PrimeReactProvider>
   );
 };
-
 export default App;
