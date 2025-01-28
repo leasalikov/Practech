d697e36de9566e1b0676e56c8b97c75003030b5c
minidzaldov <mini6731504@gmail.com>
2025-01-28 09:49:15 +0200

    working on dashboard page

--- pracTek/fronted/vite-project/src/App.tsx
+++ pracTek/fronted/vite-project/src/App.tsx
@@ -1,23 +1,11 @@
 // import React from 'react';
 import './App.css';
-import { PrimeReactProvider } from 'primereact/api'; // PrimeReact Context Provider
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Routing components
-import SignUp from './components/SingUp'; // SignUp component
-import AddCustomers from './components/B/AddCustomers'; // CreateCompany component
-// import CreateCompany from './components/B/CreateCompany'; // CreateCompany component
-
+import Dashboard from "./components/A/Dashboard";
 const App: React.FC = () => {
   return (
-    <PrimeReactProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<SignUp />} />
-          <Route path="/AddCustomers" element={<AddCustomers />} />
-          {/* <Route path="/CreateCompany" element={<CreateCompany />} /> */}
-          {/* Add more routes as needed */}
-        </Routes>
-      </Router>
-    </PrimeReactProvider>
+   <div>
+    <Dashboard></Dashboard>
+   </div>
   );
 };
 
